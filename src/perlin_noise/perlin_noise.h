cf510dfd1e97653dee382ae8ae7afb667c7051b0
jandre-d <jandre-d@student.codam.nl>
2019-11-20 17:32:36 +0100

    added noise and wood
    wood

---
+++ src/perlin_noise/perlin_noise.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   perlin_noise.h                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/18 14:47:42 by jandre-d       #+#    #+#                */
+/*   Updated: 2019/11/20 16:44:32 by jandre-d      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PERLIN_NOISE_H
+# define PERLIN_NOISE_H
+# include <stdbool.h>
+# include <stdlib.h>
+# include <math.h>
+# include "../common/common.h"
+
+typedef struct	s_perlin_noise
+{
+	int		*hash;
+	int		seed;
+	int		depth;
+	REAL	amp;
+	REAL	fin;
+	REAL	div;
+	REAL	xa;
+	REAL	ya;
+}				t_perlin_noise;
+
+void			perlin_noise_free(t_perlin_noise **p_noise);
+t_perlin_noise	*perlin_noise_init(int seed, int depth);
+REAL			perlin_noise(t_perlin_noise *var, REAL x, REAL y, REAL freq);
+
+#endif
--- src/texture/texture.h
+++ src/texture/texture.h
@@ -6,7 +6,7 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/15 12:03:10 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/11/17 17:30:36 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/11/20 17:24:24 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include "../common/common.h"
 # include "../algebra/vector/vector.h"
 # include "../../deps/libftprintf/ft_printf.h"
+# include "../perlin_noise/perlin_noise.h"
 
 typedef struct	s_texture
 {
@@ -33,5 +34,12 @@ t_texture		*get_texture(t_textures *textures, char *key);
 t_texture		*load_texture_file(t_textures *textures, char *name);
 t_textures		*texture_init();
 t_vec			uv_texel(t_textures *textures, char *key, REAL u, REAL v);
+void			texture_free(t_texture **texture);
+t_texture		*texture_new();
+
+bool			generate_textures(t_textures *textures);
+bool			generate_texture_lines(t_texture *texture);
+bool			generate_texture_wood(t_texture *texture);
+bool			generate_texture_noise(t_texture *texture);
 
 #endif
